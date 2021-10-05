a45f115152d5e323f9a769f2987aecead79c9e99
Kai Pham <haipham0115@gmail.com>
2021-10-04 23:08:46 -0700

    update for Pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,4 +4,25 @@ import os
 
 #get the path to the budget_data file
 #path = "../Resources/budget_data.csv"
-path = os.path.join('..', 'PyBank', 'Resources', 'budget_data.csv')
+path = os.path.join('..', 'PyPoll', 'Resources', 'PyPoll_Resources_election_data.csv')
+
+with open(path) as csvfile:
+	csvreader = csv.reader(csvfile)
+	header = next(csvreader)
+
+	total_votes = 0
+
+	votes = {}
+
+	for row in csvreader:
+		if row[2] in votes:
+			votes[row[2]] +=1
+		else:
+			votes[row[2]] = 1
+
+		
+		total_votes +=1
+
+	print(votes)
+
+	print(votes.items())
